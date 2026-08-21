d24b639b695691ba9985cf86b2796d3d7cc49942
nishan.paul.hello@gmail.com <nishan.paul.hello@gmail.com>
2026-08-22 04:48:28 +0600

    chore(research): add barrel export for research feature module

---
+++ app-frontend/src/features/research/index.ts
@@ -0,0 +1,6 @@
+export * from "./components/AblationLab";
+export * from "./components/FailureAnalysis";
+export * from "./components/ResearchLab";
+export * from "./components/StatisticalEval";
+export * from "./hooks/useResearchData";
+export * from "./data/researchMockData";
