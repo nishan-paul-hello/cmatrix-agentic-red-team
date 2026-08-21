7c9c4f8948951c6187dc10dc9a922c316a495e17
nishan.paul.hello@gmail.com <nishan.paul.hello@gmail.com>
2026-08-22 04:38:13 +0600

    feat(benchmarks): add feature barrel for centralized exports

---
+++ app-frontend/src/features/benchmarks/index.ts
@@ -0,0 +1,5 @@
+export * from "./components/BenchmarkDetail";
+export * from "./components/BenchmarkList";
+export * from "./components/BenchmarksHub";
+export * from "./hooks/useBenchmarksData";
+export * from "./data/benchmarksMockData";
